d6a9713a5467f327f42c5dcdd3ef252cd466e6fd
bhvy4 <khanna.micky01@gmail.com>
2023-01-12 21:23:33 +0530

    router set-up

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact US</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita a
+        repellendus accusantium omnis distinctio placeat quo quisquam obcaecati
+        similique, cupiditate nam, explicabo unde perferendis facilis sint sunt
+        sapiente laudantium nihil.
+      </p>
+    </div>
+  );
+}
